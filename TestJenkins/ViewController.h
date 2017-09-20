46db89fbebcd819b1af849baaf4d735276a03548
zhang benwei <978866241@qq.com>
2017-09-20 15:09:49 +0800

    first commit

---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by zhang benwei on 2017/9/20.
+//  Copyright © 2017年 zhangbenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestJenkins/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TestJenkins
+//
+//  Created by zhang benwei on 2017/9/20.
+//  Copyright © 2017年 zhangbenwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
